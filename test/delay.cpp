80385f2c363c8c407df1aab3055d0483313d9b33
Eirik J. Daling <eirik.daling@bekk.no>
2021-04-17 21:21:49 +0200

    Add delay test executable, including CMakeLists files.

---
+++ test/delay.cpp
@@ -0,0 +1,57 @@
+#include <q/support/literals.hpp>
+#include <q/fx/delay.hpp>
+#include <q_io/audio_stream.hpp>
+#include <q_io/audio_file.hpp>
+
+namespace q = cycfi::q;
+using namespace q::literals;
+
+struct delay_processor : q::port_audio_stream
+{
+    delay_processor(
+        q::wav_memory &wav, q::duration delay, float feedback)
+        : port_audio_stream(0, 2, wav.sps()), _wav(wav), _delay(delay, wav.sps()), _feedback(feedback)
+    {
+    }
+
+    void process(out_channels const &out)
+    {
+        auto left = out[0];
+        auto right = out[1];
+        for (auto frame : out.frames())
+        {
+            // Get the next input sample
+            auto s = _wav()[0];
+
+            // Mix the signal and the delayed signal
+            _y = s + _delay();
+
+            // Feed back the result to the delay
+            _delay.push(_y * _feedback);
+
+            // Output
+            left[frame] = s;
+            right[frame] = _y;
+        }
+    }
+
+    q::wav_memory &_wav;
+    q::delay _delay;
+    float _feedback;
+    float _y = 0.0f;
+};
+
+int main()
+{
+    q::wav_memory wav{"resources/clean.wav"};
+    delay_processor proc{wav, 350_ms, 0.85f};
+
+    if (proc.is_valid())
+    {
+        proc.start();
+        q::sleep(q::duration(wav.length()) / wav.sps());
+        proc.stop();
+    }
+
+    return 0;
+}
