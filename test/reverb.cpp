3ea6bd816d2d70befc612da30e2ac15cb58f7b15
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2021-12-08 19:23:57 +0100

    Split docker files to speed up pi deploys

--- test/reverb.cpp
+++ test/reverb.cpp
@@ -1,7 +1,8 @@
 #include <q/support/literals.hpp>
-#include <q_io/audio_stream.hpp>
 #include <q_io/audio_file.hpp>
+#include <q_io/audio_stream.hpp>
 #include <effekter/reverb.hpp>
+#include <effekter/delay.hpp>
 
 using namespace cycfi;
 using namespace q::literals;
@@ -12,40 +13,44 @@ namespace effektpedal
     {
         q::wav_memory &_wav;
         effektpedal::Reverb _reverb;
+        effektpedal::Reverb _reverb2;
+        effektpedal::Reverb _reverb3;
+        effektpedal::Delay _delay;
         float _y = 0.0f;
+        float _yy = 0.0f;
 
-        reverb_processor(q::wav_memory &wav, float roomSize)
-            : port_audio_stream(0, 2, wav.sps()),
-              _wav(wav), _reverb(wav.sps(), roomSize)
-        {
-        }
+        reverb_processor(q::wav_memory &wav)
+          : port_audio_stream(q::audio_device::get(0), 2, 44100), _wav(wav),
+              _reverb(44100, .1f), _reverb2(44100, .3f), _reverb3(44100, .4f),
+              _delay(44100, 700_ms, .3f) {}
 
-        void process(out_channels const &out)
-        {
-            auto left = out[0];
-            auto right = out[1];
-            for (auto frame : out.frames())
-            {
-                auto s = _wav()[0];
-                _y = _reverb(s);
-                left[frame] = _y;
-                right[frame] = _y;
-            }
+        void process(out_channels const &out) {
+          auto left = out[0];
+          auto right = out[1];
+          for (auto frame : out.frames()) {
+            auto s = _wav()[0];
+            _y = _delay(s) + _reverb2(s);
+            _yy = _delay(s) + _reverb3(s);
+            left[frame] = _y;
+            right[frame] = _yy;
+          }
         }
     };
 }
 
-int main()
-{
-    q::wav_memory wav{"resources/clean.wav"};
-    effektpedal::reverb_processor proc{wav, 1.0};
 
-    if (proc.is_valid())
-    {
-        proc.start();
-        q::sleep(q::duration(wav.length()) / wav.sps());
-        proc.stop();
+
+int main() {
+  q::wav_memory wav{"resources/clean.wav"};
+  effektpedal::reverb_processor proc{wav};
+
+  if (proc.is_valid()) {
+    proc.start();
+    while (true) {
+      q::sleep(1_s);
     }
+    proc.stop();
+  }
 
-    return 0;
+  return 0;
 }
